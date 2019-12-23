9e86bfd7f361dec15c589b6747a8b5e67b1dacdb
jim <jim93m@gmail.com>
2019-12-23 22:24:35 +0200

    Dataset preprocessing (classes, detoning, uppercase)

--- GreekLyricsClassifier.py
+++ GreekLyricsClassifier.py
@@ -6,8 +6,6 @@ from sklearn import metrics
 from sklearn import model_selection
 
 from sklearn import preprocessing
-from greek_stemmer import GreekStemmer
-import stopwords_guilannlp as stopwords
 
 from sklearn.feature_selection import chi2, SelectKBest
 
@@ -41,7 +39,7 @@ def filter_by_freq(df: pd.DataFrame, column: str, min_freq: int) -> pd.DataFrame
 # def remove_accents(data):
 #     return ''.join(x for x in unicodedata.normalize('NFKD', data) if x in string.printable).lower()
 
-
+'''
 df = pd.read_csv('dataset/greekLyrics_ansi.csv', delimiter=';', encoding='ANSI')
 print("------------------------------------------------------------------------------")
 print("# Dataset")
@@ -62,6 +60,49 @@ print("\n\n----------------------")
 print("# Target")
 print("----------------------")
 print(y.head())
+'''
+
+df = pd.read_csv('dataset/greekLyrics_ansi.csv', delimiter=';', encoding='iso8859_7')
+booleanDF = (df.groupby(['genre']).count()>10)
+genreToRemove = booleanDF[booleanDF['title'] == False]
+genreToRemove = genreToRemove.reset_index()
+
+for index, row in genreToRemove.iterrows():
+    delete_row = df[df['genre'] == row['genre']].index
+    df.drop(delete_row, inplace=True)
+    
+rapList = ["RAP","Hip Hop","Ελληνικό Ραπ","Ελληνικό χιπ χοπ"]
+pop_rockList = ["Ποπ", "Ελληνικό Ροκ", "Ελληνικό Ποπ"]
+traditionalList = ["Παραδοσιακό Κρήτης", "Παραδοσιακό Μικράς Ασίας", "Παραδοσιακό", "Παραδοσιακό Κύπρου", "Δημοτικό", "Νησιώτικο"]
+folkList = ["Λαικό", "Ελληνικό λαικό", "Ελληνική μπαλάντα", "Λαικό Μπαλάντα"]
+
+for index, row in df.iterrows():
+    if(row['genre'] in rapList):
+        row['genre'] = "rap"
+    elif(row['genre'] in pop_rockList):
+        row['genre'] = "pop-rock"
+    elif(row['genre'] in traditionalList):
+        row['genre'] = "traditional"
+    elif(row['genre'] in folkList):
+        row['genre'] = "folk"    
+        
+data = df[df['genre'] == "rap"]
+data = data.append(df[df['genre'] == "pop-rock"], ignore_index=True)
+data = data.append(df[df['genre'] == "traditional"], ignore_index=True)
+data = data.append(df[df['genre'] == "folk"], ignore_index=True)
+
+data = data.reset_index()
+data = data.drop(['index'], axis=1)
+
+import unicodedata
+def strip_accents(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+for index, row in data.iterrows():
+    row['lyrics'] = strip_accents(str(row['lyrics']).upper())
+
+df = data
 
 # Plot all the genres of our dataset and the number of tracks per genre
 fig = plt.figure(figsize=(8, 6))
@@ -78,7 +119,10 @@ plt.show()
 # Load greek stopwords
 with open('resources/greek_stopwords.txt', encoding='UTF-8') as f:
     greek_stopwords = [s for line in f.readlines() for s in line[:-1].split(' ')]
-    print(greek_stopwords)
+    #print(greek_stopwords)
+    
+greek_stopwords = [x.upper() for x in greek_stopwords]
+print(greek_stopwords)    
 
 tf_idf = TfidfVectorizer(sublinear_tf=True,
                         min_df=10,
